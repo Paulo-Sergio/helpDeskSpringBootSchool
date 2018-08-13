298618f47e786ae508a70c95c8cb13c4d85e6a8c
Paulo França <paulofranca.contato@gmail.com>
2018-08-13 16:34:25 -0300

    nova interaction base

--- src/main/java/br/com/paulofranca/Helpdesk/controller/InteractionController.java
+++ src/main/java/br/com/paulofranca/Helpdesk/controller/InteractionController.java
@@ -1,10 +1,37 @@
 package br.com.paulofranca.Helpdesk.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.paulofranca.Helpdesk.model.Interaction;
+import br.com.paulofranca.Helpdesk.service.InteractionService;
+
 @Controller
-@RequestMapping()
+@RequestMapping("/tickets/{tickedId}/interactions")
 public class InteractionController {
 
+	@Autowired
+	private InteractionService interactionService;
+
+	@PostMapping
+	public String save(@PathVariable("tickedId") Long tickedId, @Valid @ModelAttribute("interaction") Interaction iteraction,
+			BindingResult bindingResult, Model model) {
+
+		return null;
+	}
+
+	@DeleteMapping("{id}")
+	public String delete(@PathVariable("tickedId") Long tickedId, @PathVariable("id") Long id, Model model) {
+
+		return null;
+	}
 }
